8e8ce1d8fda41f7a87b5d5b4829c8d9ad081d5ee
sharon <143832438+rottenskittles@users.noreply.github.com>
2024-04-26 04:16:46 -0700

    js and html
    tired

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ document.addEventListener("DOMContentLoaded", function () {
       let btnVal = e.target.value;
       console.log(e.target)
       switch (btnVal) {
-        case "tavernkeep":
-          console.log("tavernkeep")
+        case "introcont":
+          console.log("introcont")
           // document.getElementById("start").classList.add("hidden");
           toggleVis('start');
-          // document.getElementById("tvkdialogue").classList.remove("hidden");
-          toggleVis('tvkdialogue')
+          // document.getElementById("gameplay").classList.remove("hidden");
+          toggleVis('gameplay');
           break;
 
       }
@@ -62,10 +62,252 @@ document.addEventListener("DOMContentLoaded", function () {
       let btnVal = e.target.value;
       console.log(e.target)
       switch (btnVal) {
+        case "tavernkeep":
+          console.log("tavernkeep")
+          // document.getElementById("start").classList.add("hidden");
+          toggleVis('gameplay');
+          // document.getElementById("tvkdialogue").classList.remove("hidden");
+          toggleVis('tvkdialogue');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
         case "cards":
           console.log("cards")
-          toggleVis('start');
+          toggleVis('gameplay');
           toggleVis('cardtable');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "backcards":
+          console.log("backcards")
+          toggleVis('cardtable');
+          toggleVis('gameplay');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "backboast":
+          console.log("backboast")
+          toggleVis('boasttable');
+          toggleVis('gameplay');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "boasters":
+          console.log("boasters")
+          toggleVis('gameplay');
+          toggleVis('boasttable');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "backtavern":
+          console.log("backtavern")
+          toggleVis('tvkdialogue');
+          toggleVis('gameplay');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "teamup":
+          console.log("teamup")
+          toggleVis('tvkdialogue');
+          toggleVis('team');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "backteamask":
+          console.log("backteamask")
+          toggleVis('team');
+          toggleVis('tvkdialogue');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "goodreason":
+          console.log("goodreason")
+          toggleVis('team');
+          toggleVis('teamyes');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "yesteam":
+          console.log("yesteam")
+          toggleVis('teamyes');
+          toggleVis('gameplay');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "badreason":
+          console.log("badreason")
+          toggleVis('team');
+          toggleVis('teamno');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "noteam":
+          console.log("noteam")
+          toggleVis('teamno');
+          toggleVis('deathorreset');
+          break;
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("gameBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (dif) {
+
+      let btnVal = dif.target.value;
+      console.log(dif.target)
+      switch (btnVal) {
+        case "tvkreset":
+          console.log("tvkreset")
+          toggleVis('deathorreset');
+          toggleVis('gameplay');
           break;
 
       }
