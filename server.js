7579c6e49f62e0395a03b1af82d32fd060dca8a2
sharon <143832438+rottenskittles@users.noreply.github.com>
2024-04-27 03:18:45 -0700

    a lot

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   //let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   //for (i = 0; i < dots.length; i++) {
-//   //  dots[i].className = dots[i].className.replace(" active", "");
-//  // }
-//   slides[slideIndex-1].style.display = "block";
-//   //dots[slideIndex-1].className += " active";
-// }
-
 function toggleVis(elementID){
   document.getElementById(elementID).classList.toggle("hidden");
 }
@@ -119,25 +90,13 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('gameplay');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "buytavern":
           console.log("buytavern")
           toggleVis('tvkdialogue');
           toggleVis('buytav');
           break;
-
+          
+        
       }
     })
   }
@@ -157,76 +116,24 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('item1details');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "buyitem1":
           console.log("buyitem1")
           toggleVis('item1details');
           toggleVis('item1confirm');
           break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
+          
         case "item1yes":
           console.log("item1yes")
           toggleVis('item1confirm');
           toggleVis('buytav');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "item1no":
           console.log("item1no")
           toggleVis('item1confirm');
           toggleVis('item1details');
           break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
+          
         case "backitem1":
           console.log("backitem1")
           toggleVis('item1details');
@@ -251,83 +158,28 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('buytav');
           toggleVis('item2details');
           break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "buyitem2":
           console.log("buyitem2")
           toggleVis('item2details');
           toggleVis('item2confirm');
           break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
-        case "item2yes":
-          console.log("item2yes")
-          toggleVis('item2confirm');
-          toggleVis('buytav');
-          break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
-        case "item2no":
-          console.log("item2no")
-          toggleVis('item2confirm');
-          toggleVis('item2details');
-          break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
-        case "backitem2":
-          console.log("backitem2")
-          toggleVis('item2details');
-          toggleVis('buytav');
-          break;
-
+      
+          case "item2yes":
+            console.log("item2yes")
+            toggleVis('item2confirm');
+            toggleVis('buytav');
+            break;
+      
+          case "item2no":
+            console.log("item2no")
+            toggleVis('item2confirm');
+            toggleVis('item2details');
+            break;
+          case "backitem2":
+            console.log("backitem2")
+            toggleVis('item2details');
+            toggleVis('buytav');
+            break;    
       }
     })
   }
@@ -346,83 +198,30 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('buytav');
           toggleVis('item3details');
           break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
-        case "buyitem3":
-          console.log("buyitem3")
-          toggleVis('item3details');
-          toggleVis('item3confirm');
-          break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
+          
+          case "buyitem3":
+            console.log("buyitem3")
+            toggleVis('item3details');
+            toggleVis('item3confirm');
+            break;
+      
         case "item3yes":
           console.log("item3yes")
           toggleVis('item3confirm');
           toggleVis('buytav');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "item3no":
           console.log("item3no")
           toggleVis('item3confirm');
           toggleVis('item3details');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "backitem3":
           console.log("backitem3")
           toggleVis('item3details');
           toggleVis('buytav');
-          break;
-
+          break;    
       }
     })
   }
@@ -442,82 +241,29 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('potion1details');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "buypotion1":
           console.log("buypotion1")
           toggleVis('potion1details');
           toggleVis('potion1confirm');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "potion1yes":
           console.log("potion1yes")
           toggleVis('potion1confirm');
           toggleVis('buytav');
-          break;
-
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
+          break;     
+          
         case "potion1no":
           console.log("potion1no")
           toggleVis('potion1confirm');
           toggleVis('potion1details');
-          break;
+          break;          
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "backpotion1":
           console.log("backpotion1")
           toggleVis('potion1details');
           toggleVis('buytav');
           break;
-
       }
     })
   }
@@ -537,68 +283,29 @@ document.addEventListener("DOMContentLoaded", function () {
           toggleVis('potion2details');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "buypotion2":
           console.log("buypotion2")
           toggleVis('potion2details');
           toggleVis('potion2confirm');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "potion2yes":
           console.log("potion2yes")
           toggleVis('potion2confirm');
           toggleVis('buytav');
           break;
 
-      }
-    })
-  }
-
-})
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByClassName("gameBtn");
-  for (button of buttons) {
-    button.addEventListener("click", function (dif) {
-
-      let btnVal = dif.target.value;
-      console.log(dif.target)
-      switch (btnVal) {
         case "potion2no":
           console.log("potion2no")
           toggleVis('potion2confirm');
           toggleVis('potion2details');
           break;
-
       }
     })
   }
 
 })
+
 document.addEventListener("DOMContentLoaded", function () {
   let buttons = document.getElementsByClassName("gameBtn");
   for (button of buttons) {
